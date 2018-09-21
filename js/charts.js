88353dcd9ebe27f69b9aeab0c0bc1bbc6d7264e4
Rodolfo Perez <rodolfobeste@gmail.com>
2018-09-21 15:08:58 +0300

    Added Donut and bars chart

--- js/charts.js
+++ js/charts.js
@@ -1,7 +1,7 @@
 
 ///////////   VARIABLES  /////////////////
 
-const area_chart = document.querySelector("#area-chart");
+
 
 ///////////   Alerts  /////////////////
 
@@ -13,32 +13,82 @@ $(document).ready($alert_message.show({
 }))
 
 $close_alert.click(() => {
-    $alert_message.hide();
- //   $close_alert.parent().remove("div");
+    $alert_message.hide({
+        duration: 800
+    });
 });
 
 
 ////////////// Line chart /////////////
 
-const myLineChart = new Chart(area_chart, {
+    new Chart(document.querySelector("#area-chart"), {
     type: 'line',
     data: {
-        label: "Traffic",
+        
         xLabels:["Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        backgroundColor: "rgb(252, 232, 255)",
-        borderColor: "rgb(252, 232, 255)",
-        borderWidth: 4,
-        pointBorderWidth: 15,
-        showLine: true,
+        
         datasets: [{
-            data: [1,3,5,4,6,2,8]
+            data: [1345,2895,1935,2101,1875,2500, 2978],
+        backgroundColor: "rgba(16, 123, 147, 0.144)",
+        borderColor: "#107B93",
+        borderWidth: 1,
+        pointBorderWidth: 5,
+        showLine: true
         }]
     },
 
+    options: {
+            legend: { display: false },
+            title:{
+                display: true,
+                text: 'TRAFFIC'
+              }
+    }
+});
+
+////////////// DONUT CHART /////////////
+
+
+new Chart(document.querySelector("#donut_chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Phones", "Tablets", "Desktop"],
+      datasets: [
+        {
+          backgroundColor: ["#3cba9f", "#8e5ea2", "#107B93"],
+          data: [650, 458, 1243]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'MOBILE USERS'
+      }
+    }
 });
 
 
+////////////// BARS CHARTS /////////////
 
-area_chart.textContent = myLineChart;
 
-
+new Chart(document.querySelector("#bar_chart"), {
+    type: 'bar',
+    data: {
+      labels: ["S", "M", "T", "W", "T", "F", "S"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#107B93","#107B93","#107B93","#107B93","#107B93","#107B93","#107B93"],
+          data: [1345,2895,1935,2101,1875,2500, 2978]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'DAILY TRAFFIC'
+      }
+    }
+});
