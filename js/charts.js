880aa8e89d8d7bc6bad910175e2cc158faaa4ec8
Rodolfo Perez <rodolfobeste@gmail.com>
2018-09-26 11:45:20 +0300

    Added functionality to traffic menu, created funtion to display area chart dinamicly, improved styling of doughnut chart

--- js/charts.js
+++ js/charts.js
@@ -1,7 +1,4 @@
 
-///////////   VARIABLES  /////////////////
-
-
 
 ///////////   Alerts  /////////////////
 
@@ -19,32 +16,74 @@ $close_alert.click(() => {
 });
 
 
-////////////// Line chart /////////////
+////////////// AREA CHART FUNCTION /////////////
 
-    new Chart(document.querySelector("#area-chart"), {
-    type: 'line',
-    data: {
-        
-        xLabels:["Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        
-        datasets: [{
-            data: [1345,2895,1935,2101,1875,2500, 2978],
-        backgroundColor: "rgba(16, 123, 147, 0.144)",
-        borderColor: "#107B93",
-        borderWidth: 1,
-        pointBorderWidth: 5,
-        showLine: true
-        }]
-    },
+const dataset_hourly = [125,100,98,67,75,86,90,101,76,65,64,32,37,09, 45, 67, 78, 69, 98, 101, 98,87,78, 89];
+const xlabels_hourly = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00",  "22:00",  "23:00"];
 
-    options: {
-            legend: { display: false },
-            title:{
-                display: false,
-                text: 'TRAFFIC'
-              }
+const dataset_daily = [1345,2895,1935,2101,1875,2500, 2978];
+const xlabels_daily = ["Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+const dataset_weekly = [5768, 4657, 5007, 5234, 4897, 4098, 5647, 6875, 6754];
+const xlabels_weekly = ["3-9", "10-16", "17-23", "24-30", "1-6", "7-13", "14-20", "21-27"];
+
+const dataset_monthly = [19834, 20234, 21039, 19578, 18039, 19010];
+const xlabels_monthly = ["May", "June", "July", "August", "September", "October"];
+
+    ////////Function to print an area chart /////////
+
+function print_area_chart (data, xlabels) {
+
+    let area_chart = new Chart(document.querySelector("#area-chart"), {
+      type: 'line',
+      data: {
+          
+          xLabels: xlabels,
+          
+          datasets: [{
+              data: data,
+              backgroundColor: "rgba(16, 123, 147, 0.144)",
+              borderColor: "#107B93",
+              borderWidth: 1,
+              pointBorderWidth: 5,
+              showLine: true
+          }]
+      },
+
+      options: {
+              legend: { display: false },
+              elements: {
+                line: {
+                    tension: 0
+                }
+      }
     }
+    });
+
+  return area_chart
+}
+
+    
+
+////////////// TIMEFRAME MENU FUNCTIONALITY /////////////
+
+const timeFrameMenu = document.querySelector("#timeframe_menu");
+const timeFrameList = document.querySelectorAll("#timeframe_menu button");
+
+timeFrameMenu.addEventListener("click", (event) => {
+  if (event.target === timeFrameList[0]) {
+    print_area_chart(dataset_hourly, xlabels_hourly);
+  } else if (event.target === timeFrameList[1]) {
+    print_area_chart(dataset_daily, xlabels_daily);
+  } else if (event.target === timeFrameList[2]) {
+    print_area_chart(dataset_weekly, xlabels_weekly);
+  } else if (event.target === timeFrameList[3]) {
+    print_area_chart(dataset_monthly, xlabels_monthly);
+  } 
 });
+
+print_area_chart(dataset_daily, xlabels_daily);
+console.log(print_area_chart(dataset_daily, xlabels_daily))
 
 ////////////// DONUT CHART /////////////
 
@@ -62,8 +101,22 @@ new Chart(document.querySelector("#donut_chart"), {
     },
     options: {
       title: {
-        display: false,
-        text: 'MOBILE USERS'
+        display: false
+      },
+      layout: {
+        padding: {
+
+            right: 50
+        }
+    },
+      legend: {
+        position: "right",
+        labels: {
+          boxWidth: 16,
+          fontSize: 16,
+          padding: 15
+          
+        }
       }
     }
 });
